a7c9111028a5ff4f605cb7bfe1ad19373b3fc8ce
bangcode13 <muhammadwahyufirmansyah@gmail.com>
2018-04-25 19:46:13 +0700

    auto play

--- orionstatic/assets/js/sequence/sequence-theme.modern-slide-in.js
+++ orionstatic/assets/js/sequence/sequence-theme.modern-slide-in.js
@@ -24,10 +24,13 @@ var sequenceElement = document.getElementById("sequence");
 // Place your Sequence options here to override defaults
 // See: http://sequencejs.com/documentation/#options
 var options = {
-  animateCanvas: false,
-  phaseThreshold: false,
-  preloader: true,
-  reverseWhenNavigatingBackwards: true
+	autoPlay:true,
+	autoPlayInterval:2000,
+	animateCanvas: false,
+	phaseThreshold: false,
+	preloader: true,
+	reverseWhenNavigatingBackwards: true
+	
 }
 
 // Launch Sequence on the element, and with the options we specified above
---
+++ slider/assets/js/sequence/mysequencetheme.js
@@ -0,0 +1,9 @@
+
+var sequenceElement = document.getElementById("sequence");
+
+var options = {
+	keyNavigation: true,
+	fadeStepWhenSkipped: false
+}
+
+var mySequence = sequence(sequenceElement, options);
