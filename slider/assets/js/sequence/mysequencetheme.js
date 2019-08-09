5edd8438085f57f292554a9b6ea9ec6da0794805
bangcode <muhammadwahyufirmansyah@gmail.com>
2019-08-10 00:48:24 +0700

    test

--- slider/assets/js/sequence/mysequencetheme.js
+++
@@ -1,9 +0,0 @@
-
-var sequenceElement = document.getElementById("sequence");
-
-var options = {
-	keyNavigation: true,
-	fadeStepWhenSkipped: false
-}
-
-var mySequence = sequence(sequenceElement, options);
